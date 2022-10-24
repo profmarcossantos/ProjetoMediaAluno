21ad5eac1157e0951ca40274c98b0e81dce548ff
Marcos R. Santos <marcos.santos@atitus.edu.br>
2022-10-24 21:06:55 -0200

    Letras Caixa Alta

--- calculaMedia.py
+++ calculaMedia.py
@@ -1,5 +1,5 @@
-nota1 = int(input("Nota1: "))
-nota2 = int(input("Nota2: "))
+nota1 = int(input("NOTA1: "))
+nota2 = int(input("NOTA2: "))
 
 media = (nota1+nota2)/2
-print("A média é:", media)
+print("A MÉDIA É:", media)
