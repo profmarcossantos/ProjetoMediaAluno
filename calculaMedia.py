24afe3ddd55dedbff01ba4d80f1f3b469fe68ecc
diegotrevisancc <diego.trevisan.cc@gmail.com>
2022-10-24 21:12:32 -0300

    Media de 3 alunos - Diego

--- calculaMedia.py
+++ calculaMedia.py
@@ -1,6 +1,6 @@
 print ("Sejam Bem-Vindos!")
 nota1 = int(input("NOTA1: "))
 nota2 = int(input("NOTA2: "))
-
-media = (nota1+nota2)/2
+nota3 = int(input("NOTA3: "))
+media = (nota1+nota2+nota3)/3
 print("A MÉDIA É:", media)
